fb06a4f9f18cb0f9673a314fc3e9e62ae6108b06
adunlop1234 <dunlop.arthur@gmail.com>
2021-05-31 17:19:28 +0100

    Added all links in the nav bar.

--- create.py
+++ create.py
@@ -5,7 +5,7 @@ templateLoader = jinja2.FileSystemLoader(searchpath="./templates")
 templateEnv = jinja2.Environment(loader=templateLoader)
 
 # Loop over each file and create
-template_files = ["index.html"]
+template_files = ["index.html", "draw.html", "table.html", "betting.html"]
 for template_file in template_files:
     template = templateEnv.get_template(template_file)
     template_render = template.render()
